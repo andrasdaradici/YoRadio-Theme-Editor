04e26c71968a1f6c8e5977159680c3913361816e
András Daradici <90605554+andrasdaradici@users.noreply.github.com>
2024-08-29 20:52:11 +0300

    Add files via upload

--- mytheme.h
+++ mytheme.h
@@ -1,5 +1,5 @@
 // File created with YoRadio Theme Editor created by András Daradics
-// File last modified: 8/25/2024 3:50:18 PM
+// File last modified: 8/26/2024 4:52:49 PM
 // GitHub: https://github.com/andrasdaradici/YoRadio-Theme-Editor
 // Itch.io: https://andrasdaradici.itch.io/yoradio-theme-editor
 #ifndef _my_theme_h
@@ -8,38 +8,38 @@
 #define ENABLE_THEME
 #ifdef  ENABLE_THEME
 
-#define COLOR_BACKGROUND     0,  87,  0
-#define COLOR_STATION_NAME     0,  205,  0
-#define COLOR_STATION_BG    0,  87,  0
-#define COLOR_STATION_FILL    0,  118,  0
-#define COLOR_SNG_TITLE_1    205,  6,  0
-#define COLOR_SNG_TITLE_2    127,  6,  0
-#define COLOR_WEATHER    220,  220,  220
-#define COLOR_VU_MAX    0,  202,  0
-#define COLOR_VU_MIN    0,  124,  0
-#define COLOR_CLOCK    0,  180,  0
-#define COLOR_CLOCK_BG    0,  46,  0
-#define COLOR_SECONDS    0,  230,  0
-#define COLOR_DAY_OF_W    0,  182,  0
-#define COLOR_DATE    0,  228,  46
+#define COLOR_BACKGROUND     0,  0,  0
+#define COLOR_STATION_NAME     255,  255,  255
+#define COLOR_STATION_BG    0,  0,  249
+#define COLOR_STATION_FILL    0,  0,  249
+#define COLOR_SNG_TITLE_1    255,  255,  0
+#define COLOR_SNG_TITLE_2    255,  0,  255
+#define COLOR_WEATHER    0,  255,  0
+#define COLOR_VU_MAX    255,  0,  0
+#define COLOR_VU_MIN    0,  0,  255
+#define COLOR_CLOCK    255,  255,  255
+#define COLOR_CLOCK_BG    0,  0,  0
+#define COLOR_SECONDS    255,  255,  0
+#define COLOR_DAY_OF_W    255,  0,  202
+#define COLOR_DATE    108,  119,  255
 #define COLOR_BUFFER    157,  0,  79
-#define COLOR_IP    152,  174,  115
-#define COLOR_VOLUME_VALUE    255,  255,  255
-#define COLOR_RSSI    112,  255,  0
-#define COLOR_VOLBAR_OUT    0,  0,  0
-#define COLOR_VOLBAR_IN    255,  255,  255
+#define COLOR_IP    0,  255,  0
+#define COLOR_VOLUME_VALUE    255,  0,  0
+#define COLOR_RSSI    255,  255,  0
+#define COLOR_VOLBAR_OUT    255,  0,  0
+#define COLOR_VOLBAR_IN    255,  255,  0
 #define COLOR_DIGITS    100, 100, 255
 #define COLOR_DIVIDER    255,  255,  255
-#define COLOR_BITRATE    0,  205,  0
+#define COLOR_BITRATE    0,  255,  255
 #define COLOR_HEAP    255, 168, 162
-#define COLOR_PL_CURRENT    0, 0, 0
-#define COLOR_PL_CURRENT_BG    91, 118, 255
-#define COLOR_PL_CURRENT_FILL    91, 118, 255
-#define COLOR_PLAYLIST_0    255, 255, 255
-#define COLOR_PLAYLIST_1    255, 255, 255
-#define COLOR_PLAYLIST_2    255, 255, 255
-#define COLOR_PLAYLIST_3    255, 255, 255
-#define COLOR_PLAYLIST_4    255, 255, 255
+#define COLOR_PL_CURRENT    0,  0,  255
+#define COLOR_PL_CURRENT_BG    255,  20,  86
+#define COLOR_PL_CURRENT_FILL    255,  255,  255
+#define COLOR_PLAYLIST_0    255,  198,  255
+#define COLOR_PLAYLIST_1    255,  0,  0
+#define COLOR_PLAYLIST_2    136,  23,  235
+#define COLOR_PLAYLIST_3    0,  255,  255
+#define COLOR_PLAYLIST_4    255,  255,  255
 
 
 #endif
